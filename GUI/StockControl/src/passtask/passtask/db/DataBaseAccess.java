611e067fd71071b9fde23d6a0ca238ca6f6a9f10
Devon <froggerman330@gmail.com>
2016-04-08 09:45:16 +1000

    Get the product catalog and add a new item to the list of products.

--- GUI/StockControl/src/passtask/passtask/db/DataBaseAccess.java
+++ GUI/StockControl/src/passtask/passtask/db/DataBaseAccess.java
@@ -46,6 +46,7 @@ public class DataBaseAccess
      * 
      * @return a connection to the database.
      * @throws SQLException
+     *             if there is a problem connecting to the database.
      */
     private static Connection getConnection() throws SQLException
     {
@@ -62,8 +63,8 @@ public class DataBaseAccess
     /**
      * Gets the ID, Name, Current Stock Level and Price of all items.
      * 
-     * @return a two dimensional array containing ID, Name, Current Stock Levels
-     *         and Price of all items.
+     * @return a two dimensional array containing ID (barcode), Name, Current
+     *         Stock Levels and Price of all items.
      */
     public static Object[][] getPOSInterfaceData()
     {
@@ -111,8 +112,9 @@ public class DataBaseAccess
      * Updates the database with the information pertaining to any sale.
      * 
      * @param data
-     *            a multidimensional object array containing sale item ID and
-     *            updated stock amount (old current - amount sold).
+     *            a multidimensional object array containing sale item ID
+     *            (barcode) and updated stock amount (old current - amount
+     *            sold).
      */
     public void makeSale(Object[][] data)
     {
@@ -120,7 +122,7 @@ public class DataBaseAccess
 	PreparedStatement updateSaleHistory = null;
 
 	String updateInventoryString = "UPDATE " + databaseName + ".inventory SET stock = ? WHERE barcode = ?";
-	String updateHistoryString = "INSERT INTO " + databaseName + "(barcode, updated_stock) VALUES (?, ?)";
+	String updateHistoryString = "INSERT INTO SALE_HISTORY (barcode, updated_stock) VALUES (?, ?)";
 
 	try(Connection conn = getConnection())
 	{
@@ -136,6 +138,84 @@ public class DataBaseAccess
 		updateSaleHistory.setInt(1, (int) data[i][0]);
 		updateSaleHistory.setInt(2, (int) data[i][1]);
 		updateSaleHistory.executeUpdate();
+
+		conn.commit();
+	    }
+	}
+	catch(SQLException e)
+	{
+	    e.printStackTrace();
+	}
+    }
+
+    /**
+     * Gets the ID (barcode), name, and description from the database for the
+     * add a new item interface.
+     * 
+     * @return a two dimensional array containing ID (barcode), Name and
+     *         Description.
+     */
+    public static Object[][] getItemCatalogue()
+    {
+	Statement getData = null;
+	Object[][] returnData = null;
+
+	String selectData = "SELECT barcode, prod_name, description FROM product";
+
+	try(Connection conn = getConnection())
+	{
+	    getData = conn.createStatement();
+	    ResultSet results = getData.executeQuery(selectData);
+	    int rowCount = 0, i = 0;
+
+	    if(results.last())
+	    {
+		rowCount = results.getRow();
+		results.beforeFirst();
+	    }
+
+	    returnData = new Object[rowCount][3];
+
+	    while(results.next())
+	    {
+		returnData[i][0] = results.getInt(0);
+		returnData[i][1] = results.getString(1);
+		returnData[i][2] = results.getString(2);
+		i++;
+	    }
+	}
+	catch(SQLException e)
+	{
+	    e.printStackTrace();
+	}
+
+	return returnData;
+    }
+
+    /**
+     * Add a new item to the product catalog.
+     * 
+     * @param data
+     *            a two dimensional array containing the new product name, the
+     *            description and the price.
+     */
+    public void addItem(Object[][] data)
+    {
+	PreparedStatement updateProduct = null;
+
+	String updateProductString = "INSERT INTO PRODUCT (prod_name, description, price) VALUES (?, ?, ?)";
+
+	try(Connection conn = getConnection())
+	{
+	    conn.setAutoCommit(false);
+	    updateProduct = conn.prepareStatement(updateProductString);
+
+	    for(int i = 0; i < data.length; i++)
+	    {
+		updateProduct.setString(1, (String) data[i][0]);
+		updateProduct.setString(2, (String) data[i][1]);
+		updateProduct.setDouble(3, (int) data[i][2]);
+		updateProduct.executeUpdate();
 
 		conn.commit();
 	    }
